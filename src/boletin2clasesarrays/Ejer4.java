440371b060d6b9b2740abd58d98d4cd0e2c40bbd
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-11-11 23:12:12 +0100

    Acabado

--- src/boletin2clasesarrays/Ejer4.java
+++ src/boletin2clasesarrays/Ejer4.java
@@ -13,15 +13,39 @@ public class Ejer4 {
 		// Creo el escaner para poder usarlo.
 		Scanner sc = new Scanner(System.in);
 		
+		// Variable que almacenará la posición cuando vayamos a invertir el orden.
+		int temp;
+		
 		// Indico al usuario lo que tiene que hacer.
 		System.out.println("Introduce las puntuaciones de cada jugador.");
 		
+		// For para recoger la puntuacion de cada jugador en cada posición de la tabla.
 		for (int i = 0; i < 8; i++) {
 			System.out.println("Puntuación del jugaro " + (i + 1) + ":");
 			table[i] = sc.nextInt();
 		}
 		
-		Arrays.
+		// Ordeno la tabla para empear de menor a mayor.
+		Arrays.sort(table);
+		
+		// Reinvierto el orden para que sea de mayor a menor.
+        for (int i = 0; i < table.length / 2; i++) {
+        	
+        	// Guardo temporalmente el valor de la posición i
+            temp = table[i];
+            
+            // copio el valor de la posicion opuesta (desde el final) en la posición i.
+            table[i] = table[table.length - 1 - i];
+            
+            // clooco el valor temporal en la posición opuesta, completando el intercambio
+            table[table.length - 1 - i] = temp;
+        }
+        
+        // Imprimo el array ordenado de mayor a menor
+        System.out.println("Puntuaciones de mayor a menor:");
+        for (int score : table) {
+            System.out.println(score);
+        }
 		
 		
 		// Cierro uso de escaner.
