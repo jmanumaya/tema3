2a0dd2f990f2de40c86661af793963c8f46b0d3c
jmanumaya <jm.maya@iesnervion.es>
2024-11-12 13:32:18 +0100

    Falta comentarios

---
+++ src/boletin2clasesarrays/Ejer7.java
@@ -0,0 +1,21 @@
+package boletin2clasesarrays;
+
+import java.util.Arrays;
+
+public class Ejer7 {
+
+	public static void main(String[] args) {
+		
+		int inicio = 0;
+		int[] tabla = new int[55];
+		
+		for (int i = 1; i<=10; i++) {
+			Arrays.fill(tabla, inicio, inicio+i, i);
+			inicio += i;
+		}
+		
+		System.out.println(Arrays.toString(tabla));
+
+	}
+
+}
