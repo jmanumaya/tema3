7e1dd74295d5c49be3c6630a7655da12ced5a0a4
jmanumaya <jm.maya@iesnervion.es>
2024-11-13 09:47:32 +0100

    Acabado

---
+++ src/boletinarraysbidimensionales/Ejer1.java
@@ -0,0 +1,31 @@
+package boletinarraysbidimensionales;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		
+		// Creo el tablero.
+		int tablero[][] = new int[3][6];
+		
+		// Añado los valores del tablero en cada posición
+		tablero[0][0] = 0;
+		tablero[0][1] = 30;
+		tablero[0][2] = 2;
+		tablero[0][5] = 5;
+		tablero[1][0] = 75;
+		tablero[1][4] = 0;
+		tablero[2][2] = -2;
+		tablero[2][3] = 9;
+		tablero[2][5] = 11;
+		
+		// For para imprimir el tablero.
+		for (int i= 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[0].length; j++) {
+				System.out.print(tablero [i][j] + "|");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ src/boletinarraysbidimensionales/Ejer2.java
@@ -0,0 +1,67 @@
+package boletinarraysbidimensionales;
+
+import java.util.*;
+
+public class Ejer2 {
+	
+	public static void main(String[] args) {
+		
+		// Creo la tabla que almacenará cada nota
+		double notas[][] = new double [4][5];
+		
+		// Creo el escaner para poder usarlo
+		Scanner sc = new Scanner(System.in);
+		
+		// Inicializo estas variables para poder usarlas luego para la media.
+		double suma = 0.0;
+		double media = 0.0;
+		
+		// Primer for para introducir las notas de cada alumno (cada alumno es una fila y cada asignatura una columna)
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Alumno " + (i + 1) + " introduce tus notas.");
+			for (int j = 0; j < notas[0].length; j++) {
+				System.out.println("Nota " + (j + 1) + ":");
+				notas[i][j] = sc.nextDouble();
+			}
+		}
+		
+		// Imprimo dichas notas o la tabla en sí.
+		for (int i = 0; i < notas.length; i++) {
+			System.out.print("Alumno " + (i + 1) + ":\t");
+			for (int j = 0; j < notas[0].length; j++) {
+				System.out.print(notas[i][j] + "|");
+			}
+			System.out.println();
+		}
+		
+		// Ordeno la tabla para obtener luego la máxima (última) y la mínima (primera).
+		Arrays.sort(notas[0]);
+		Arrays.sort(notas[1]);
+		Arrays.sort(notas[2]);
+		Arrays.sort(notas[3]);
+		
+		System.out.println();
+		
+		// Imprimo la nota mínima, máxima y la nota media.
+		for (int i = 0; i < notas.length;i++) {
+			System.out.println("La nota mínima del Alumno " + (i + 1) +": " + notas[i][0]);
+			System.out.println("La nota máxima del Alumno " + (i + 1) +": " + notas[i][notas.length]);
+				
+				// for para imprimir la nota media.
+		        for (int j = 0; j < notas[i].length; j++) {
+		        	// Sumo cada nota de cada fila empezando en 0 como la i del for del principio.
+		            suma += notas[i][j];
+		        }
+		    // Una vez obtenido la suma, hago la media dividiendo dicha suma entre el largo de cada fila [i]
+		    media = suma / notas[i].length;
+		    // Imprimo la nota media una vez calculada.
+		    System.out.println("La media del Alumno " + (i + 1) +": " + media+"\n");
+		    // Reseteo dichos valores para la siguiente.
+		    suma = 0;
+		    media = 0;
+		}
+		
+		// Cierro uso de escaner.
+		sc.close();
+	}
+}
