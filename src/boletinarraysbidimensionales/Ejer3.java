7d6146c9a35c8732033b6f8f9a94af7fee70c0e5
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-11-14 11:54:01 +0100

    Acabado

--- src/boletinarraysbidimensionales/Ejer3.java
+++ src/boletinarraysbidimensionales/Ejer3.java
@@ -12,30 +12,36 @@ public class Ejer3 {
 		//Variablre que va a almacenar las columnas.
 		int y;
 		
+		//Creo el escaner para poder usarlo
 		Scanner sc = new Scanner(System.in);
 		
+		// Creo el tablero.
+		int[][] tablero;
+		
+		//Indico al usuario que debe hace y que hará el programa.
 		System.out.println("Crea una tabla, me tienes que dar las filas y columnas y el programa la rellenara sola");
+		
+		//Pido al usuario el número de las filas y columnas
 		System.out.print("Dime cuantas filas quieres: ");
 		x = sc.nextInt();
 		System.out.println("Dime cuantas columnas quieres: ");
 		y = sc.nextInt();
 		
-		int tablero[][] = new int[x][y];
+		// Asigno los valores al tablero.
+		tablero = new int[x][y];
 		
+		// For para ir recorriendo cada posición, ir asignando su correspondiente valor e ir imprimiendo.
 		for (int i=0; i < tablero.length; i++) {
-			for (int j = 0; j < tablero.length; j++) {
+			for (int j = 0; j < tablero[i].length; j++) {
 				tablero[i][j] = 10 * i + j;
-			}
-		}
-		
-		// Imprimo dichas notas o la tabla en sí.
-		for (int i = 0; i < tablero.length; i++) {
-			for (int j = 0; j < tablero[0].length; j++) {
-				System.out.print(tablero[i][j] + "|");
+				System.out.print(tablero[i][j] + "\t");
 			}
 			System.out.println();
 		}
-
+		// Aquí había hecho otro for para imprimir pero he visto que era igual que este anterior asi que lo he aprobechado 
+		//para a la vez que se asigna, se muestre.
+		
+		// Cierro uso de escaner.
+		sc.close();
 	}
-
 }
