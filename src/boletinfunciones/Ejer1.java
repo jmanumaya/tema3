b5ca482eead600a6af16cf4360c8eeb1c17f6887
jmanumaya <jm.maya@iesnervion.es>
2024-11-15 14:38:55 +0100

    Primer push de esto (nose que acabo de hacer)

---
+++ src/boletinfunciones/Ejer1.java
@@ -0,0 +1,17 @@
+package boletinfunciones;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		
+		int tabla[] = {1,2,3,4};
+		
+
+	}
+	
+	static void suma(int tablas) {
+		
+
+	}
+
+}
