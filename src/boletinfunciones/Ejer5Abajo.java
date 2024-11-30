206095705d2ed159f6071620405d11b2adb08315
jmanumaya <jm.maya@iesnervion.es>
2024-12-01 00:40:59 +0100

    Acabado

--- src/boletinfunciones/Ejer5Abajo.java
+++ src/boletinfunciones/Ejer5Abajo.java
@@ -1,37 +1,71 @@
 package boletinfunciones;
 
+import java.util.Scanner;
+
 public class Ejer5Abajo {
-	
-	public static int[][] gira90(int tablaInicio[][]){
-		int tabla[][] = new int[4][4];
-		int aux = 0;
-		int i = 4;
-		int j = 4;
-		
 
-		for (int x = 0; x < tabla.length; x++) {			
-			for (int z = 0; z < tabla[x].length; z++) {		
-				aux = tabla[i][j];
-				tabla[i][j] = tabla[x][j];		
-				j--;
-			}		
-			--i;
-		}	
-		
-		return tabla;
-	}
+    // Función que gira la matriz 90 grados.
+    public static int[][] girar90(int tablaInicio[][]) {
+    	// Tamaño de la matriz.
+        int n = tablaInicio.length; 
+        int[][] resultado = new int[n][n];
 
-	public static void main(String[] args) {
-	
-		int tabla[][] = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		int respuesta[][] = gira90(tabla);
-		
-		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla[i].length; j++) {
-				System.out.println(respuesta[i][j] + "\t");
-			}
-			System.out.println();
-		}
-	}
+        // Rotar la matriz
+        // Recorro las filas de la matriz original
+        for (int i = 0; i < n; i++) { 
+        	// Recorro las columnas de la matriz original
+            for (int j = 0; j < n; j++) { 
+            	// Rotación de 90 grados
+                resultado[j][n - 1 - i] = tablaInicio[i][j]; 
+            }
+        }
+        // Devuelvo la matriz rotada.
+        return resultado; 
+    }
 
+    public static void main(String[] args) {
+    	// Creo el escaner.
+        Scanner scanner = new Scanner(System.in);
+
+        // Pido al usuario el tamaño de la matriz.
+        System.out.print("Introduce el tamaño de la matriz NxN (N): ");
+        int n = scanner.nextInt();
+
+        // Creo la matriz
+        int[][] tabla = new int[n][n];
+
+        // Rellenamos la matriz con los datos del usuario.
+        System.out.println("Introduce los elementos de la matriz:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                tabla[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Mostramos la matriz original.
+        System.out.println("\nMatriz original:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(tabla[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Llamamos a la función para girar la matriz.
+        int[][] respuesta = girar90(tabla);
+
+        // Mostramos la matriz rotada.
+        System.out.println("\nMatriz rotada 90 grados:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(respuesta[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        // Cierro uso de escáner.
+        scanner.close(); 
+    }
 }
+
